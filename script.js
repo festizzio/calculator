5d817db7567d55149c2d221b1248e873f86b2f5f
Sean <festizzio@gmail.com>
2022-10-20 07:01:22 -0700

    Add helper function to call one of the 4 operator functions

--- script.js
+++ script.js
@@ -6,10 +6,25 @@ function subtract(x, y) {
     return x - y;
 }
 
-function multiple(x, y) {
+function multiply(x, y) {
     return x * y;
 }
 
 function divide(x, y) {
     return x / y;
+}
+
+function operate(x, y, operator) {
+    switch(operator) {
+        case "+":
+            return add(x,y);
+        case "-":
+            return subtract(x,y);
+        case "*":
+            return multiply(x,y);
+        case "/":
+            return divide(x,y);
+        default:
+            return undefined;
+    }
 }
